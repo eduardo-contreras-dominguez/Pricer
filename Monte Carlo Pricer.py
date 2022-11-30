9521f83fff21a040f07a8b6d77e503ba67aa04d2
educontreras <edu.contreras@gmail.com>
2022-11-30 10:06:08 +0100

    Now that we have possible asset path, compute payoff of an european vanilla option and average + present value

--- Monte Carlo Pricer.py
+++ Monte Carlo Pricer.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
 import math
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
-def simulate_asset_path(S0, risk_free_rate, volatility, NTS, T, N=1000):
+def simulate_asset_path(S0, risk_free_rate, volatility, NTS, T, N=10000):
     """
     Simulating realizations of log-normal risk-neutral random walk
 
@@ -20,7 +22,7 @@ def simulate_asset_path(S0, risk_free_rate, volatility, NTS, T, N=1000):
     # We will start at S0 for every simulation
     realization_array[0, :] = [S0 for simulation in range(N)]
     dt = T / NTS
-    for simulation in range(N):
+    for simulation in tqdm(range(N)):
         random_variable = np.random.normal(0, 1, NTS)
         for timestep in range(1, NTS):
             realization_array[timestep, simulation] = realization_array[timestep - 1, simulation] \
@@ -29,7 +31,23 @@ def simulate_asset_path(S0, risk_free_rate, volatility, NTS, T, N=1000):
                                                                      timestep])
     return realization_array
 
+def Compute_Vanilla_European_Option_Value(S0, risk_free_rate, volatility, Expiry, Strike, Option_Type = "C"):
+    """
+    Will compute the value of an European Vanilla Option using the asset paths on last method.
+
+    :param S0: Initial asset price
+    :param risk_free_rate: spot risk-free rate
+    :param volatility: historical vol
+    :param Expiry: maturity of contract
+    :param Strike: Strike of the option
+    :param Option_Type: Call or put
+
+    :return: PV of payoff's average.
+    """
+
 
 if __name__ == "__main__":
-    df = simulate_asset_path(S0=100, risk_free_rate=0.05, volatility=0.2, NTS=1000, T=1)
+    df = simulate_asset_path(S0=100, risk_free_rate=0.05, volatility=0.1, NTS=1000, T=1)
+    #plt.plot(df)
+    #plt.show()
     print("Hello")
